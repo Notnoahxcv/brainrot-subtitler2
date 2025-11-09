521bff8b69d0a4a0d59b2388a53f7800a5d4e69a
Notnoahxcv <noahtuan23@gmail.com>
2025-11-09 18:53:36 +0500

    Update App.jsx

--- brainrot-subtitler/src/App.jsx
+++ brainrot-subtitler/src/App.jsx
@@ -1,1 +1,12 @@
-<PLACEHOLDER>
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>🧠 Brainrot Subtitler</h1>
+      <p>Your app is live! Now we just need to add the real features.</p>
+    </div>
+  );
+}
+
+export default App;
